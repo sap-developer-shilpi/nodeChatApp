12fbd190a7e3d5041b1b558d69b1f94e3f1e36ea
Shilpi Sharma <sap.developer.shilpi@gmail.com>
2018-01-18 14:30:16 +0100

    add connect and disconnect events

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 var app = express();
+const server = http.createServer(app);
+var io = socketIO(server);
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port, () =>{
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+
+server.listen(port, () =>{
     console.log(`server up on port ${port}`);
 });
